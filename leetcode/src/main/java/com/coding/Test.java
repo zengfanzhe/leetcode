14c5fa965c067c6666ffd9ed98d6aadeb862bae6
zengfanzhe <hello_zfz@163.com>
2020-06-08 21:19:50 +0800

    just test2

--- leetcode/src/main/java/com/coding/Test.java
+++ leetcode/src/main/java/com/coding/Test.java
@@ -2,6 +2,6 @@ package com.coding;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Integer.valueOf("1"));
+        System.out.println(Integer.valueOf("2"));
     }
 }
